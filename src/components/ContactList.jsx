bd67c70386a8676f538d7c877de93d07634e945e
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-07-06 13:32:44 +0300

    Update ContactList.jsx

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -9,7 +9,7 @@ class ContactList extends Component {
   render() {
     return (
       <ul>
-        {this.props.filter == ''
+        {this.props.filter.length === 0
           ? this.props.contacts.map(item => {
               return (
                 <li key={nanoid()}>
